d1fb0ea07e0a550cb18d2e7be5074a86d24e5dfb
Tilen Gašparič <tilen623@gmail.com>
2025-05-03 16:56:19 +0200

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+def kmeans(slika, k=3, iteracije=10):
+    '''Izvede segmentacijo slike z uporabo metode k-means.'''
+    pass
+
+def meanshift(slika, velikost_okna, dimenzija):
+    '''Izvede segmentacijo slike z uporabo metode mean-shift.'''
+    pass
+
+def izracunaj_centre(slika, izbira, dimenzija_centra, T):
+    '''Izračuna centre za metodo kmeans.'''
+    pass
+
+if __name__ == "__main__":
+    pass
