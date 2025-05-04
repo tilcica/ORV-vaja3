68707f27cd6577430ba895f5a74da42ea0aac006
Tilen Gašparič <tilen623@gmail.com>
2025-05-04 11:57:59 +0200

    added choice for rocno

--- main.py
+++ main.py
@@ -9,7 +9,43 @@ def meanshift(image, velikost_okna=30, dimenzija=3):
     pass
 
 def izracunaj_centre(image, choice, centerDimension, T):
-    pass
+    centers = []
+    if centerDimension == 3:
+        features = image.reshape((-1, 3))
+    elif centerDimension == 5:
+        h, w = image.shape[:2]
+        Y, X = np.mgrid[0:h, 0:w]
+        features = np.hstack((image.reshape((-1, 3)), np.dstack((X, Y)).reshape((-1, 2))))
+    
+    features = np.float32(features)
+    
+    if choice == 'r':
+        displayImg = image.copy()
+    
+        def selectCenter(event, x, y, flags, param):
+            if event == cv.EVENT_LBUTTONDOWN:
+                if centerDimension == 3:
+                    center = image[y, x]
+                else:
+                    center = np.append(image[y, x], [x, y])
+                centers.append(center)
+                cv.circle(displayImg, (x, y), 5, (0, 0, 255), -1)
+                cv.imshow('center select', displayImg)
+    
+        cv.imshow('center select', displayImg)
+        cv.setMouseCallback('center select', selectCenter)
+        
+        while True:
+            key = cv.waitKey(1) & 0xFF
+            if key == 27:  # ESC key
+                break
+        
+        cv.destroyAllWindows()
+
+    elif choice == 'n':
+        pass
+    
+    return np.array(centers)
 
 
 if __name__ == "__main__":
