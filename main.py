f364dd8da3d825025d66e4c8dc67403ecfcb4ae7
Tilen Gašparič <tilen623@gmail.com>
2025-05-04 11:55:34 +0200

    SCRUM-14 main function

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import cv2 as cv
+import numpy as np
+import random
 
-def kmeans(slika, k=3, iteracije=10):
-    '''Izvede segmentacijo slike z uporabo metode k-means.'''
+def kmeans(image, k=3, iterations=10, choice='r', T=10):
     pass
 
-def meanshift(slika, velikost_okna, dimenzija):
-    '''Izvede segmentacijo slike z uporabo metode mean-shift.'''
+def meanshift(image, velikost_okna=30, dimenzija=3):
     pass
 
-def izracunaj_centre(slika, izbira, dimenzija_centra, T):
-    '''Izračuna centre za metodo kmeans.'''
+def izracunaj_centre(image, choice, centerDimension, T):
     pass
+
 
 if __name__ == "__main__":
-    pass
+    image = cv.imread('image.png')
+    #segmented = meanshift(image, velikost_okna=30, dimenzija=5)
+    segmented = kmeans(image)
+    cv.imshow('Segmented', segmented)
+    cv.waitKey(0)
